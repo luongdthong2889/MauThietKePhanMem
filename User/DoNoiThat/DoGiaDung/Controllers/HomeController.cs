3f0fff928f2c9d3590a3745c5f50c8763627b991
luongdthong2889 <luong.dthong@gmail.com>
2022-04-27 01:28:07 +0700

    done

---
+++ User/DoNoiThat/DoGiaDung/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using DoGiaDung.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoGiaDung.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBGiaDungEntities db = new DBGiaDungEntities();
+        // GET: Product
+        public ActionResult Index()     
+        {
+            return View(db.PRODUCTs.Take(12).ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
